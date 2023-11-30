d641221a7061a79a6aa80747ff03e42c4bb6a3a1
Jan Gleixner <jan.gleixner@gmail.com>
2023-11-30 18:56:43 +0100

    switch to roxygen md

--- R/geom_pointdensity.R
+++ R/geom_pointdensity.R
@@ -46,7 +46,7 @@ stat_pointdensity <- function(mapping = NULL,
 }
 
 
-#' ggproto class, see \code{\link[ggplot2]{Stat}}
+#' ggproto class, see [ggplot2::Stat()]
 #' @format NULL
 #' @usage NULL
 #' @export
@@ -177,44 +177,44 @@ StatPointdensity <- ggproto("StatPointdensity", Stat,
 #'
 #' @aliases geom_pointdensity stat_pointdensity StatPointdensity
 #' @param mapping Set of aesthetic mappings created by
-#' \code{\link[=aes]{aes()}} or \code{\link[=aes_]{aes_()}}. If specified and
-#' \code{inherit.aes = TRUE} (the default), it is combined with the default
-#' mapping at the top level of the plot. You must supply \code{mapping} if
+#' [`aes()`][aes] or [`aes_()`][aes_]. If specified and
+#' `inherit.aes = TRUE` (the default), it is combined with the default
+#' mapping at the top level of the plot. You must supply `mapping` if
 #' there is no plot mapping.
 #' @param data The data to be displayed in this layer. There are three options:
 #'
-#' If \code{NULL}, the default, the data is inherited from the plot data as
-#' specified in the call to \code{\link[=ggplot]{ggplot()}}.
+#' If `NULL`, the default, the data is inherited from the plot data as
+#' specified in the call to [`ggplot()`][ggplot].
 #'
-#' A \code{data.frame}, or other object, will override the plot data. All
+#' A `data.frame`, or other object, will override the plot data. All
 #' objects will be fortified to produce a data frame. See
-#' \code{\link[=fortify]{fortify()}} for which variables will be created.
+#' [`fortify()`][fortify] for which variables will be created.
 #'
-#' A \code{function} will be called with a single argument, the plot data. The
-#' return value must be a \code{data.frame}, and will be used as the layer
-#' data. A \code{function} can be created from a \code{formula} (e.g. \code{~
-#' head(.x, 10)}).
+#' A `function` will be called with a single argument, the plot data. The
+#' return value must be a `data.frame`, and will be used as the layer
+#' data. A `function` can be created from a `formula` (e.g. `~
+#' head(.x, 10)`).
 #' @param stat The statistical transformation to use on the data for this
 #' layer, as a string.
 #' @param position Position adjustment, either as a string, or the result of a
 #' call to a position adjustment function.
-#' @param \dots Other arguments passed on to \code{\link[=layer]{layer()}}.
-#' This includes \code{adjust}, a multiplicate bandwidth adjustment used to
+#' @param \dots Other arguments passed on to [`layer()`][layer].
+#' This includes `adjust`, a multiplicate bandwidth adjustment used to
 #' adjust the distance threshold to consider two points as neighbors, i.e. the
 #' radius around points in which neighbors are counted. For example,
-#' \code{adjust = 0.5} means use half of the default. Other arguments may be
-#' aesthetics, used to set an aesthetic to a fixed value, like \code{shape =
-#' 17} or \code{size = 3}. They may also be parameters to the paired geom/stat.
-#' @param na.rm If \code{FALSE}, the default, missing values are removed with a
-#' warning. If \code{TRUE}, missing values are silently removed.
+#' `adjust = 0.5` means use half of the default. Other arguments may be
+#' aesthetics, used to set an aesthetic to a fixed value, like `shape =
+#' 17` or `size = 3`. They may also be parameters to the paired geom/stat.
+#' @param na.rm If `FALSE`, the default, missing values are removed with a
+#' warning. If `TRUE`, missing values are silently removed.
 #' @param show.legend logical. Should this layer be included in the legends?
-#' \code{NA}, the default, includes if any aesthetics are mapped. \code{FALSE}
-#' never includes, and \code{TRUE} always includes. It can also be a named
+#' `NA`, the default, includes if any aesthetics are mapped. `FALSE`
+#' never includes, and `TRUE` always includes. It can also be a named
 #' logical vector to finely select the aesthetics to display.
-#' @param inherit.aes If \code{FALSE}, overrides the default aesthetics, rather
+#' @param inherit.aes If `FALSE`, overrides the default aesthetics, rather
 #' than combining with them. This is most useful for helper functions that
 #' define both data and aesthetics and shouldn't inherit behaviour from the
-#' default plot specification, e.g. \code{\link[=borders]{borders()}}.
+#' default plot specification, e.g. [`borders()`][borders].
 #' @author Lukas P.M. Kremer
 #' @references https://GitHub.com/LKremer/ggpointdensity
 #' @examples

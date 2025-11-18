6a4b5cebfbf601e05055d5c7e27bbbc3acfe589c
LKremer <l-kremer@web.de>
2025-11-18 11:41:35 +0100

    update docs & imports

--- R/ggpointdensity-package.R
+++ R/ggpointdensity-package.R
@@ -3,6 +3,7 @@
 
 ## usethis namespace: start
 #' @import ggplot2
+#' @importFrom MASS bandwidth.nrd kde2d
 #' @useDynLib ggpointdensity, count_neighbors_, .registration = TRUE
 ## usethis namespace: end
 NULL
